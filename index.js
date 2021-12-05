edabd77613b5452d94c4ef4fda820bb38a3126ee
xavicoder <70905472+xavi-coder@users.noreply.github.com>
2021-12-05 14:08:20 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const body = document.getElementById("body")
+const greeting = document.getElementById("greeting")
+
+const christmasBtn = document.getElementById("christmas")
+christmasBtn.addEventListener("click", christmassify)
+
+const snowBtn = document.getElementById("snow")
+snowBtn.addEventListener("click", snowify)
+
+function christmassify() {
+  body.classList.remove("snow");
+  body.classList.add("christmas");
+  greeting.innerHTML = `Merry Christmas! 🎅`
+}
+
+function snowify() {
+  body.classList.remove("christmas");
+  body.classList.add("snow");
+  greeting.innerHTML = `Let it Snow! ☃️`
+}
